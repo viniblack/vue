82795b624012d0de3a7a184d9e0b304ad045d475
Vini Black <viniibl4ck@gmail.com>
2023-08-10 20:30:38 -0300

    primeiros components

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'BarraLateral'
+})
+</script>
+
+<template>
+    <header>
+        <h1>
+            <img src="../assets/logo.png" alt="logo">
+        </h1>
+    </header>
+</template>
+
+<style>
+header {
+    padding: 1rem;
+    background: #0d3b66;
+    width: 100%;
+    height: 100vh;
+}
+
+@media only screen and (max-width: 768px) {
+    header {
+        padding: 2.5rem;
+        height: auto;
+    }
+}
+</style>
