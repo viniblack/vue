ec297b9a170f07f0dd921631fe607fc45f265420
Vini Black <viniibl4ck@gmail.com>
2023-08-11 10:01:11 -0300

    criando rotas

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-import '@fortawesome/fontawesome-free/css/all.css'
+import "@fortawesome/fontawesome-free/css/all.css";
+import roteador from "./router";
 
-createApp(App).mount('#app')
+createApp(App).use(roteador).mount("#app");
---
+++ src/router/index.ts
@@ -0,0 +1,15 @@
+import { RouteRecordRaw, createRouter, createWebHashHistory } from "vue-router";
+import Tarefas from '../views/Tarefas.vue';
+
+const rotas: RouteRecordRaw[] = [{
+    path: '/',
+    name: 'Tarefas',
+    component: Tarefas,
+}];
+
+const roteador = createRouter({
+    history: createWebHashHistory(),
+    routes: rotas
+});
+
+export default roteador;
