627b98424a5eea4f8a1659381f6a6049e41689b3
Vini Black <viniibl4ck@gmail.com>
2023-08-11 10:09:42 -0300

    criando rota de projetos

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,23 @@
 import { RouteRecordRaw, createRouter, createWebHashHistory } from "vue-router";
-import Tarefas from '../views/Tarefas.vue';
+import Tarefas from "../views/Tarefas.vue";
+import Projetos from "../views/Projetos.vue";
 
-const rotas: RouteRecordRaw[] = [{
-    path: '/',
-    name: 'Tarefas',
+const rotas: RouteRecordRaw[] = [
+  {
+    path: "/",
+    name: "Tarefas",
     component: Tarefas,
-}];
+  },
+  {
+    path: "/projetos",
+    name: "Projetos",
+    component: Projetos,
+  },
+];
 
 const roteador = createRouter({
-    history: createWebHashHistory(),
-    routes: rotas
+  history: createWebHashHistory(),
+  routes: rotas,
 });
 
 export default roteador;
---
+++ src/views/Projetos.vue
@@ -0,0 +1,19 @@
+<template>
+<h1>Projetos</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ViewProjetos',
+  components: {
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+  }
+});
+</script>
