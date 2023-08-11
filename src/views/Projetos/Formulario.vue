b71dbeb58cab3aaad12aadfb663c840b5911a9d6
Vini Black <viniibl4ck@gmail.com>
2023-08-11 12:21:30 -0300

    organizando as mutations de projeto

--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -14,6 +14,7 @@
 
 <script lang="ts">
 import { useStore } from "@/store";
+import { ADICIONA_PROJETO, ALTERA_PROJETO } from "@/store/tipoMutacoes";
 import { defineComponent } from "vue";
 
 export default defineComponent({
@@ -37,9 +38,9 @@ export default defineComponent({
   methods: {
     salvar() {
       if (this.id) {
-        this.store.commit('ALTERA_PROJETO', { id: this.id, nome: this.nomeDoProjeto })
+        this.store.commit(ALTERA_PROJETO, { id: this.id, nome: this.nomeDoProjeto })
       } else {
-        this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto)
+        this.store.commit(ADICIONA_PROJETO, this.nomeDoProjeto)
       }
       this.nomeDoProjeto = "";
       this.$router.push('/projetos')
